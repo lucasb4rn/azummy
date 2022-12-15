cf4eb965a7082496fdf035114cc0cddb7e17955e
lucasb4rn <lucas.b4rn@gmail.com>
2022-12-15 19:59:33 -0300

    att

--- src/main/java/com/lucas/azumy/config/CorsConfig.java
+++ src/main/java/com/lucas/azumy/config/CorsConfig.java
@@ -1,19 +1,16 @@
 package com.lucas.azumy.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Component
+
 public class CorsConfig {
 
-    @Bean
+    
     public WebMvcConfigurer corsConfigurer() {
 
         return new WebMvcConfigurer() {
-        	@Override
+        	
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
